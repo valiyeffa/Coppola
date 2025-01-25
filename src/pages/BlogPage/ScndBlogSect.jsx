edf027f80af292515e8f80b7d0d2c3a69888700e
valiyeffa <firuzeveliyeva2@gmail.com>
2025-01-25 16:53:36 +0400

    blog

--- src/pages/BlogPage/ScndBlogSect.jsx
+++ src/pages/BlogPage/ScndBlogSect.jsx
@@ -18,29 +18,36 @@ const ScndBlogSect = () => {
                         </div>
                     </div>
                 </div>
+
                 <div className="blog-page-body">
                     <div className="row">
                         <div className="col-md-6">
-                            <div className="blog-card mb-3">
-                                <img src="https://coppola.qodeinteractive.com/wp-content/uploads/2021/11/blog-list-22.jpg" className="card-img-top" />
+                            <div className="blog-card">
+                                <div className="blog-card-img">
+                                    <img src="https://coppola.qodeinteractive.com/wp-content/uploads/2021/11/blog-list-22.jpg" className="card-img-top" />
+                                </div>
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
+                                    <p className="textbox-ctg"><span>Adventure</span> NOVEMBER 30</p>
+                                    <h5 className="card-title">Film by beverly santos</h5>
                                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                                 </div>
                             </div>
                         </div>
                         <div className="col-md-6">
-                            <div className="blog-card mb-3">
-                                <img src="https://coppola.qodeinteractive.com/wp-content/uploads/2021/11/blog-list-22.jpg" className="card-img-top" />
+                            <div className="blog-card">
+                                <div className="blog-card-img">
+                                    <img src="https://coppola.qodeinteractive.com/wp-content/uploads/2021/11/blog-list-22.jpg" className="card-img-top" />
+                                </div>
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
+                                    <p className="textbox-ctg"><span>Adventure</span> NOVEMBER 30</p>
+                                    <h5 className="card-title">Film by beverly santos</h5>
                                     <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text"><small className="text-body-secondary">Last updated 3 mins ago</small></p>
                                 </div>
                             </div>
                         </div>
-
+                        <BlogCard/>
+                        <BlogCard/>
+                        <BlogCard/>
                     </div>
                 </div>
             </div>
