20db45a0954cada7e042cbfe4b7592cd5a1fa41f
valiyeffa <firuzeveliyeva2@gmail.com>
2025-01-28 21:45:14 +0400

    bask

--- src/pages/Basket/Basket.jsx
+++ src/pages/Basket/Basket.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import img from '../../../src/pages/Movies/images/product-3-300x425.jpg'
-
+import { FaHeart, FaTrash } from "react-icons/fa";
+import { Bounce, toast, ToastContainer } from 'react-toastify';
 const Basket = () => {
+    const [count, setCount] = useState(1);
+
+    if (count < 0) {
+        setCount(0);
+        toast.warn('🦄 Wow so easy!', {
+            position: "top-center",
+            autoClose: 2500,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+            transition: Bounce,
+        });
+    }
+
     return (
         <div className='basket-page'>
             <div className="container-fluid">
                 <p className='basket-title'>HOME / CART</p>
+                <ToastContainer
+                    position="top-center"
+                    autoClose={2500}
+                    hideProgressBar={false}
+                    newestOnTop={false}
+                    closeOnClick={false}
+                    rtl={false}
+                    pauseOnFocusLoss
+                    draggable
+                    pauseOnHover
+                    theme="light"
+                    transition="Bounce"
+                />
                 <div className="row">
                     <div className="col-md-8">
                         <div className="basket-prod-sect">
@@ -20,8 +51,22 @@ const Basket = () => {
                                         </div>
                                         <div className="col-md-10">
                                             <div className="card-body">
-                                                <h5 className="card-title">ANESA</h5>
+                                                <div className="card-body-frst">
+                                                    <h5 className="card-title">ANESA</h5>
+                                                    <p className='card-price'>$12</p>
+                                                </div>
                                                 <p className="card-text"><del className='sale'>$25</del> $12 <span className='sale-text'>On Sale</span><span className='new-text'>New</span></p>
+                                                <div className="card-body-bottom">
+                                                    <div className="card-quantity">
+                                                        <button onClick={() => setCount(count - 1)} className='btn btn-dark'>-</button>
+                                                        <span className='count'>{count}</span>
+                                                        <button onClick={() => setCount(count + 1)} className='btn btn-dark'>+</button>
+                                                    </div>
+                                                    <div className="card-remove-fav">
+                                                        <button className='btn fav-btn'><FaHeart /> Save</button>
+                                                        <button className='btn del-btn'><FaTrash /> Delete</button>
+                                                    </div>
+                                                </div>
                                             </div>
                                         </div>
                                     </div>
