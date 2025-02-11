2f6722fac492b6445c39a06811d861e8058f456b
valiyeffa <firuzeveliyeva2@gmail.com>
2025-02-11 13:16:01 +0400

    upd

--- src/pages/CheckoutPage/Checkout.jsx
+++ src/pages/CheckoutPage/Checkout.jsx
@@ -1,12 +1,18 @@
 import { ConfigProvider, Select } from 'antd';
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useCart } from 'react-use-cart';
 
 const Checkout = () => {
     const { items, cartTotal } = useCart();
-    const [card, setCard] = useState(false);
+    const [card, setCard] = useState(true);
     const [cash, setCash] = useState(false);
+    const [active, setActive] = useState('active-btn');
     const [countryCode, setCountryCode] = useState("+994");
+    const [check, setCheck] = useState(false);
+    const [monthCard, setMonth] = useState();
+
+
+
     return (
         <div className='checkout-page'>
             <div className="container-fluid">
@@ -17,7 +23,7 @@ const Checkout = () => {
                     <div className="col-md-6">
                         <div className="checkout-body left-side">
                             <div className="checkout-info my-5">
-                                <b>Shipping Information</b>
+                                <b>Billing Details</b>
                                 <form className='my-4'>
                                     <div className="mb-3">
                                         <label className="form-label">Full name*</label>
@@ -78,19 +84,18 @@ const Checkout = () => {
                                         <label className="form-label">State*</label>
                                         <input placeholder='Enter state' type="text" className="form-control" />
                                     </div>
-                                    <div className="payment-mthod mb-3">
+                                    <div className="payment-method mb-3">
                                         <label className="form-label">Choose Payment method:</label><br />
                                         <button onClick={() => {
-                                            setCash(true);
-                                            setCard(false);
-                                        }} type='button' className='btn btn-outline-dark'>Card on Delivery</button>
-                                        <button onClick={() => {
-                                            setCard(true);
                                             setCash(false);
-                                        }} type='button' className='btn btn-outline-dark'>Cash on Delivery</button>
+                                            setCard(true);
+                                        }} type='button' className={`btn card-cash-btn ${card == true ? active : ''}`}>Card on Delivery</button>
+                                        <button onClick={() => {
+                                            setCard(false);
+                                            setCash(true);
+                                        }} type='button' className={`btn card-cash-btn mx-2 ${cash == true ? active : ''}`}>Cash on Delivery</button>
                                     </div>
-
-                                    {card == false ?
+                                    {card == true ?
                                         <div className="card-on-delivery mb-3">
                                             <div className="credit-card">
                                                 <div className="card-num">
@@ -98,19 +103,27 @@ const Checkout = () => {
                                                     <input placeholder='1234 1234 1234 1234' type="number" className="form-control" />
                                                 </div>
                                                 <div className="year-cvv d-flex">
-                                                    <input placeholder='MM/YY' type="number" className="form-control" />
+                                                    <input placeholder='MM/YY' onChange={(e) => { setMonth(e.target.value) }} type="number" className="form-control" />
                                                     <input placeholder='CVC' type="number" className="form-control" />
                                                 </div>
                                             </div>
                                         </div> :
-                                        <div className="cash-on-delivery mb-3">
+                                        <div className="cash-on-delivery mb-3" >
                                             <div className="zip-code">
                                                 <label className="form-label">ZIP Code</label>
                                                 <input type="number" className="form-control" />
                                             </div>
                                         </div>
                                     }
-                                    <button type="submit" className="btn btn-dark">Submit</button>
+
+                                    <div className="mb-3 form-check">
+                                        <input onClick={() => { check == false ? setCheck(true) : setCheck(false) }} type="checkbox" className="form-check-input" />
+                                        <label className="form-check-label">I have read and agree to the Terms and Conditions.</label>
+                                    </div>
+                                    {check == true ?
+                                        <button type="submit" className="btn btn-submit">Order now</button> :
+                                        <button disabled type="submit" className="btn btn-submit">Order now</button>
+                                    }
                                 </form>
                             </div>
                         </div>
